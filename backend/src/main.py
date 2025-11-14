a0cee7176c3bce3f90f5da2e94ad858933e732e4
marwa-khafagy <99355272+marwa-khafagy@users.noreply.github.com>
2025-11-13 20:56:14 -0500

    Revert "feat: setup google oauth [CP-52] (#5)"
    This reverts commit 0ec1c51ecd8098ad140386bf4427020c25fda005.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,11 @@
-from src import create_app
+from flask import Flask
 
-app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    # app.run(debug=True, host='0.0.0.0', port=8000)
+def temp():
+    app = Flask(__name__)
+    print("Test")
+
+    return app
+
+
+app = temp()
