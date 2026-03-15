ed38fe9e01ad9103e3fae630e62e169c1ff1b7a2
Selina <selinakimdev@outlook.com>
2026-03-14 22:02:08 -0400

    fix: integrate user dropdown menu & logout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -4,89 +4,170 @@ import { HelpIcon } from "@/assets/icons/HelpIcon";
 import { HomeIcon } from "@/assets/icons/HomeIcon";
 import { OpenBookIcon } from "@/assets/icons/OpenBookIcon";
 import { ProfileIcon } from "@/assets/icons/ProfileIcon";
+import { CText } from "@/components/common/CText";
 import { COLORS } from "@/constants/colors";
 import { SHADOWS } from "@/constants/shadows";
 import { useAuth } from "@/context/AuthContext";
-import { Tabs } from "expo-router";
-import { Pressable, View } from "react-native";
-
-function HeaderRight() {
-  const { signOut, user } = useAuth();
-
-  const handleProfilePress = () => {
-    signOut(); // TODO: temporary, replace with profile page later
-  };
-
-  if (!user) return null;
-
-  return (
-    <Pressable
-      onPress={handleProfilePress}
-      style={{ marginHorizontal: 15, width: 38 }}
-    >
-      <ProfileIcon />
-    </Pressable>
-  );
-}
+import { router, Tabs, usePathname } from "expo-router";
+import { useState } from "react";
+import { Modal, Pressable, View } from "react-native";
 
 export default function TabLayout() {
+  const { signOut, user } = useAuth();
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleSettings = () => {
+    setMenuVisible(false);
+    router.push("/(tabs)/settings"); // Navigate to settings within tabs
+  };
+
+  const handleLogout = () => {
+    setMenuVisible(false);
+    signOut();
+  };
+
+  const handleProfilePress = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  function HeaderRight() {
+    if (!user) return null;
+
+    return (
+      <Pressable
+        onPress={handleProfilePress}
+        style={{ marginHorizontal: 15, width: 38, height: 38 }}
+      >
+        <ProfileIcon />
+      </Pressable>
+    );
+  }
+
+  function UserDropdownMenu() {
+    const pathname = usePathname();
+    const showSettingsOption = pathname !== "/settings";
+    return (
+      <Modal
+        visible={menuVisible}
+        transparent
+        onRequestClose={() => setMenuVisible(false)}
+      >
+        <Pressable
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+          onPress={() => setMenuVisible(false)}
+        />
+        <View
+          style={{
+            position: "absolute",
+            top: 120,
+            left: 0,
+            right: 0,
+            backgroundColor: COLORS.backgroundPrimary,
+            paddingVertical: 10,
+            ...SHADOWS.default,
+            borderBottomWidth: 8,
+            borderBottomColor: COLORS.accent.primary,
+          }}
+        >
+          {showSettingsOption && (
+            <Pressable
+              onPress={handleSettings}
+              style={{
+                paddingVertical: 14,
+                alignItems: "center",
+                borderBottomWidth: 1,
+                borderBottomColor: "#D0D0D0",
+              }}
+            >
+              <CText style={{ top: -3 }} bold>
+                User Settings
+              </CText>
+            </Pressable>
+          )}
+          <Pressable
+            onPress={handleLogout}
+            style={{
+              paddingVertical: 14,
+              alignItems: "center",
+            }}
+          >
+            <CText style={{ top: 5 }} bold>
+              Logout
+            </CText>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  }
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: COLORS.accent.primary,
-          ...SHADOWS.default,
-        },
-        headerTintColor: COLORS.text.primary,
-        headerTitle: () => (
-          <View style={{ width: 100 }}>
-            <AppLogo />
-          </View>
-        ),
-        headerRight: () => <HeaderRight />,
-        tabBarActiveTintColor: COLORS.accent.secondary, // TODO?
-        tabBarInactiveTintColor: COLORS.icon.outlinePrimary,
-        tabBarShowLabel: false,
-        tabBarInactiveBackgroundColor: COLORS.accent.primary,
-        tabBarActiveBackgroundColor: COLORS.accent.primary,
-        tabBarLabelPosition: "beside-icon",
-        tabBarStyle: { borderTopWidth: 0 },
-        sceneStyle: {
-          backgroundColor: COLORS.backgroundPrimary,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color }) => <HomeIcon stroke={color} />,
+    <>
+      <Tabs
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            height: 120,
+            backgroundColor: COLORS.accent.primary,
+            ...SHADOWS.default,
+          },
+          headerTintColor: COLORS.text.primary,
+          headerTitle: () => (
+            <View style={{ width: 100 }}>
+              <AppLogo />
+            </View>
+          ),
+          headerRight: () => <HeaderRight />,
+          tabBarActiveTintColor: COLORS.accent.secondary, // TODO?
+          tabBarInactiveTintColor: COLORS.icon.outlinePrimary,
+          tabBarShowLabel: false,
+          tabBarInactiveBackgroundColor: COLORS.accent.primary,
+          tabBarActiveBackgroundColor: COLORS.accent.primary,
+          tabBarLabelPosition: "beside-icon",
+          tabBarStyle: { borderTopWidth: 0 },
+          sceneStyle: {
+            backgroundColor: COLORS.backgroundPrimary,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="revision"
-        options={{
-          tabBarIcon: ({ color }) => <OpenBookIcon stroke={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="decks"
-        options={{
-          tabBarIcon: ({ color }) => <DecksIcon stroke={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="help"
-        options={{
-          tabBarIcon: ({ color }) => <HelpIcon stroke={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          href: null, // Hide from tab bar
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ color }) => <HomeIcon stroke={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="revision"
+          options={{
+            tabBarIcon: ({ color }) => <OpenBookIcon stroke={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="decks"
+          options={{
+            tabBarIcon: ({ color }) => <DecksIcon stroke={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="help"
+          options={{
+            tabBarIcon: ({ color }) => <HelpIcon stroke={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            href: null, // Hide from tab bar
+          }}
+        />
+      </Tabs>
+      {menuVisible && <UserDropdownMenu />}
+    </>
   );
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,37 +1,12 @@
-import { AppLogo } from "@/assets/AppLogo";
-import { ProfileIcon } from "@/assets/icons/ProfileIcon";
-import { CText } from "@/components/common/CText";
 import { COLORS } from "@/constants/colors";
-import { SHADOWS } from "@/constants/shadows";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { Arimo_400Regular, Arimo_700Bold } from "@expo-google-fonts/arimo";
 import { useFonts } from "expo-font";
-import { router, Stack } from "expo-router";
-import { useState } from "react";
-import {
-  ActivityIndicator,
-  Modal,
-  Pressable,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 function RootLayoutNav() {
   const { isLoading } = useAuth();
-
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const handleSettings = () => {
-    setMenuVisible(false);
-    router.push("/(tabs)/settings"); // Navigate to settings within tabs
-  };
-
-  const handleLogout = () => {
-    setMenuVisible(false);
-    // Add your logout logic here
-    console.log("Logout pressed");
-    router.push("/(auth)/login"); // Redirect to login after logout
-  };
 
   // Show loading screen while checking auth
   if (isLoading) {
@@ -50,82 +25,10 @@ function RootLayoutNav() {
   }
 
   return (
-    <>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: true,
-            headerStyle: {
-              backgroundColor: COLORS.accent.primary,
-              ...SHADOWS.default,
-            },
-            headerTintColor: COLORS.text.primary,
-            headerTitle: () => (
-              <View style={{ width: 100 }}>
-                <AppLogo />
-              </View>
-            ),
-            headerRight: () => (
-              <Pressable
-                onPress={() => setMenuVisible(true)}
-                style={{ marginHorizontal: 15, width: 38, height: 38 }}
-              >
-                <ProfileIcon />
-              </Pressable>
-            ),
-          }}
-        />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
-      <Modal
-        visible={menuVisible}
-        transparent
-        animationType="fade"
-        onRequestClose={() => setMenuVisible(false)}
-      >
-        <Pressable style={{ flex: 1 }} onPress={() => setMenuVisible(false)}>
-          <View
-            style={{
-              position: "absolute",
-              top: 65,
-              left: 0,
-              right: 0,
-              backgroundColor: COLORS.backgroundPrimary,
-              paddingVertical: 10,
-              ...SHADOWS.default,
-              borderBottomWidth: 8,
-              borderBottomColor: COLORS.accent.primary,
-            }}
-          >
-            <TouchableOpacity
-              onPress={handleSettings}
-              style={{
-                paddingVertical: 14,
-                alignItems: "center",
-                borderBottomWidth: 1,
-                borderBottomColor: "#D0D0D0",
-              }}
-            >
-              <CText style={{ top: -3 }} bold>
-                User Settings
-              </CText>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={handleLogout}
-              style={{
-                paddingVertical: 14,
-                alignItems: "center",
-              }}
-            >
-              <CText style={{ top: 5 }} bold>
-                Logout
-              </CText>
-            </TouchableOpacity>
-          </View>
-        </Pressable>
-      </Modal>
-    </>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
 
